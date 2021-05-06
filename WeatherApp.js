59dddc6c91cbdc270b34853d29c72a92c4f8beec
Pabelara <80460781+Pabelara@users.noreply.github.com>
2021-05-06 23:56:06 +0100

    Updated JS Unit Conversion

--- WeatherApp.js
+++ WeatherApp.js
@@ -50,11 +50,14 @@ function search(event) {
 
   function showTemperature(response) {
     event.preventDefault();
+
+    celsiusTemperature = response.data.main.temp;
+
     /* City Name */ document.querySelector("#city").innerHTML =
       response.data.name;
     /* Current Temperature */ document.querySelector(
       "#temperature"
-    ).innerHTML = Math.round(response.data.main.temp);
+    ).innerHTML = Math.round(celsiusTemperature);
     /* Humidity */ document.querySelector(
       "#humidity"
     ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
@@ -68,11 +71,15 @@ function search(event) {
       response.data.weather[0].description;
 
     let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute(
+    /* Icon Image */ iconElement.setAttribute(
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    /* Icon Description */ iconElement.setAttribute(
+      "alt",
+      response.data.weather[0].description
+    );
+
     let apiKey = "935d274474278d462bed68ee689c049b";
     let units = "metric";
     let city = searchCity.value;
@@ -81,14 +88,14 @@ function search(event) {
   axios.get(apiUrl).then(showTemperature);
 }
 
-let searchForm = document.querySelector(".search-form");
-searchForm.addEventListener("submit", search);
+let celsiusTemperature = null;
 
 function convertToFahrenheit(event) {
   event.preventDefault();
+  let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
   let temperatureElement = document.querySelector("#temperature");
   let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = 66;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -97,9 +104,11 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = 19;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+let searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", search);
